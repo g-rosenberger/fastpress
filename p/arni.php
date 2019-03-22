c8aab95f4bd6b3c9a16892b7706f81cfb178314b
Arnau Jane <penre12@gmail.com>
2019-03-22 15:06:59 +0100

    Create arni.php

---
+++ p/arni.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="sjs">
+    <meta name="author" content="bsodlover">
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon" />
+    
+    <link rel="stylesheet" href="../assets/bootstrap.css">
+
+    <title>arni</title>
+
+  </head>
+
+  <body> 
+<?php require_once('../header.php'); ?> 
+
+<main class="container lead my-3"><div class="jumbotron"><strong><i><h1 class="display-3">arni</h1></i></strong><strong><h2 class="lead">sjs</h2></strong><p><i>Date:2019-03-22:01:47:13pm</i></p><img class="img-fluid"  style="width: 100%; display: block;"  src="img/"></img></div><p>d</p></main> 
+<?php require_once('../similar.php'); ?>
+<?php require_once('../footer.php'); ?> 
+  <script src="../assets/jquery.js"></script>
+  <script src="../assets/popper.js"></script>
+  <script src="../assets/bootstrap.js"></script>
+  </body>
+
+</html>
+
